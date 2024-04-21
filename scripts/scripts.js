b456fb91550423b70d3a23b699e4df3b4910b3aa
Nathan Gurrin-Smith <nathan.gurrinsmith@outlook.com>
2024-04-21 09:52:55 -0400

    Updated some of the explanation and changed notation in the code.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,71 +15,6 @@ function central_difference(f, x0, dt) {
     return (f(x0+dt) - f(x0-dt))/2/dt
 }
 
-//////////////
-/// LINALG ///
-//////////////
-class Vec2 {
-    constructor(x0, x1) {
-        this.x0 = x0
-        this.x1 = x1
-    }
-
-    scalarAdd(a) {
-        return Vec2(this.x0 + a, this.x1 + a)
-    }
-
-    vecAdd(v) {
-        return Vec2(this.x0 + v.x0, this.x1 + v.x1)
-    }
-
-    scalarMul(a) {
-        return Vec2(a * this.x0, a * this.x1)
-    }
-
-    norm() {
-        return Math.sqrt(this.x0**2 + this.x1**2)
-    }
-}
-
-class Mat22 {
-    constructor(x00, x01, x10, x11) {
-        this.x00 = x00
-        this.x01 = x01
-        this.x10 = x10
-        this.x11 = x11
-    }
-
-    det() {
-        return this.x00 * this.x11 - this.x01 * this.x10
-    }
-
-    scalarMul(a) {
-        return Mat22(a*this.x00, a*this.x01, a*this.x10, a*this.x11)
-    }
-
-    vecMul(v) {
-        return Vec2(this.x00 * v.x0 + this.x01 * v.x1, this.x10 * v.x0 + this.x11 * v.x1)
-    }
-
-    matMul(M) {
-        m00 = this.x00 * M.x00 + this.x01 * M.x10
-        m01 = this.x00 * M.x01 + this.x01 * M.x11
-        m10 = this.x10 * M.x00 + this.x11 * M.x10
-        m11 = this.x10 * M.x01 + this.x11 * M.x11
-        return Mat22(m00, m01, m10, m11) 
-    }
-
-    inverse() {
-        d = this.det()
-        return Mat22(this.x11, this.x01, this.x10, this.x11).scalarMul(-d)
-    }
-
-    transpose() {
-        return Mat22(this.x00, this.x10, this.x01, this.x11)
-    }
-}
-
-
 ////////////////////////
 /// OPTION FUNCTIONS ///
 ////////////////////////
@@ -107,26 +42,26 @@ function delta(V, A, v, t) {
 /////////////////////////////
 /// CREDIT RISK FUNCTIONS ///
 /////////////////////////////
-function compare_sigmaV(V, vV, S, vS, delt) {
-    return vS * S - vV * V * delt
+function compare_sigmaV(V, vV, E, vE, delt) {
+    return vE * E - vV * V * delt
 }
 
-function compare_S(S, S0) {
-    return S - S0
+function compare_E(E, E0) {
+    return E - E0
 }
 
-function computeJacobian(V, vV, A, t, vS) {
+function computeJacobian(V, vV, A, t, vE) {
     const [d1, d2] = dVals(V, A, V, t)
     const nd1 = n(d1)
     const Nd1 = N(d1)
 
-    SdV = Nd1 // delta
-    Sdv = V * nd1 * Math.sqrt(t) // vega
+    EdV = Nd1 // delta
+    Edv = V * nd1 * Math.sqrt(t) // vega
 
-    gdV = (vS - vV) * Nd1 - nd1/Math.sqrt(t)
-    gdv = vS * Sdv - V * (Nd1 + nd1*d2)
+    gdV = (vE - vV) * Nd1 - nd1/Math.sqrt(t)
+    gdv = vE * Edv - V * (Nd1 + nd1*d2)
 
-    return math.matrix([[SdV, Sdv], [gdV, gdv]])
+    return math.matrix([[EdV, Edv], [gdV, gdv]])
 }
 
 function findX(s, v, w, D) {
@@ -140,13 +75,13 @@ function findX(s, v, w, D) {
     return (-b + Math.sqrt(b**2 - 4*a*c))/(2*a)
 }
 
-function findPDLStep(D, V, vV, A, t, vS, S0) {
-    let J = computeJacobian(V, vV, A, t, vS)
+function findPDLStep(D, V, vV, A, t, vE, E0) {
+    let J = computeJacobian(V, vV, A, t, vE)
     let JT = math.transpose(J)
-    let S = priceOption(V, A, vV, t)
+    let E = priceOption(V, A, vV, t)
     let delt = delta(V, A, vV, t)
-    let f1 = compare_S(S, S0)
-    let f2 = compare_sigmaV(V, vV, S, vS, delt)
+    let f1 = compare_E(E, E0)
+    let f2 = compare_sigmaV(V, vV, E, vE, delt)
     let nfx = math.matrix([-f1, -f2])
 
     let d_gn = math.multiply(math.multiply(math.inv(math.multiply(JT, J)), JT), nfx)
@@ -169,7 +104,7 @@ function findPDLStep(D, V, vV, A, t, vS, S0) {
     return dk.toArray()
 }
 
-function solve(V0, v0, A, t, S0, vS, max_iter, D) {
+function solve(V0, v0, A, t, E0, vE, max_iter, D) {
     let tol = 0.0000001
     let V = V0
     let vV = v0
@@ -177,7 +112,7 @@ function solve(V0, v0, A, t, S0, vS, max_iter, D) {
     let dvV = 10
     let i = 0
     while (i < max_iter && Math.abs(dV) > tol && Math.abs(dvV) > tol) {
-        [dV, dvV] = findPDLStep(D, V, vV, A, t, vS, S0)
+        [dV, dvV] = findPDLStep(D, V, vV, A, t, vE, E0)
         V = V + dV
         vV = vV + dvV
         i++
@@ -201,30 +136,31 @@ function getProbabilitiesOfDefault(V, v, A, max_t) {
 }
 
 function findSolutions() {
-    let S0 = document.getElementById("equity").value
-    let vS = document.getElementById("equity-volatility").value
+    let E0 = document.getElementById("equity").value
+    let vE = document.getElementById("equity-volatility").value
     let K = document.getElementById("debt").value
     let r = document.getElementById("risk-free-rate").value
     let max_t = document.getElementById("num-years").value
     let max_iter = document.getElementById("max-iter").value
-    S0 = Number(S0)
-    vS = Number(vS) / 100
+    E0 = Number(E0)
+    vE = Number(vE) / 100
     K = Number(K)
     r = Number(r) / 100
     max_t = Number(max_t)
     max_iter = Number(max_iter)
     let t = 1
     let A = getStrike(K, r, t)
-    let V0 = S0 + K
-    let v0 = vS
+    let V0 = E0 + K
+    let v0 = vE
 
     let D = 1
 
-    let [V, vV] = solve(V0, v0, A, t, S0, vS, max_iter, D)
+    let [V, vV] = solve(V0, v0, A, t, E0, vE, max_iter, D)
     let probs = getProbabilitiesOfDefault(V, vV, A, max_t)
     document.getElementById("assets").textContent = math.round(V, 4)
     document.getElementById("asset-volatility").textContent = math.round(vV * 100, 4)
-    document.getElementById("equity-output").textContent = math.round(priceOption(V, A, vV, t), 4)
+    //document.getElementById("equity-output").textContent = math.round(priceOption(V, A, vV, t), 4)
+    document.getElementById("equity-output").textContent = Math.abs(priceOption(V, A, vV, t) - E0).toPrecision(4)
     document.getElementById("delta").textContent = math.round(delta(V, A, vV, t), 4)
     document.getElementById("pdefault").textContent = probs.join("%, ") + "%"
 }
